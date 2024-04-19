9f305aefcb6b15a6b2cd21436ef4a61f775eb318
MeyiGi <130828110+MeyiGi@users.noreply.github.com>
2024-04-19 12:48:48 +0600

    Time: 247 ms (57.30%), Space: 27.9 MB (6.49%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,43 +1,32 @@
-class Solution {
-public:
-    int numIslands(vector<vector<char>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        
-        unordered_set<string> visited;
-        int res = 0;
-        
-        function<void(int, int)> dfs = [&](int i, int j) {
-            if (grid[i][j] == '1') {
-                visited.insert(to_string(i) + "," + to_string(j));
-            } else {
-                return;
-            }
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        def dfs(i, j):
+            if grid[i][j] == "1":
+                visited.add((i, j))
+            else:
+                return
             
-            if (i > 0 && visited.find(to_string(i - 1) + "," + to_string(j)) == visited.end()) {
-                dfs(i - 1, j);
-            }
-            if (j > 0 && visited.find(to_string(i) + "," + to_string(j - 1)) == visited.end()) {
-                dfs(i, j - 1);
-            }
-            if (i != m - 1 && visited.find(to_string(i + 1) + "," + to_string(j)) == visited.end()) {
-                dfs(i + 1, j);
-            }
-            if (j != n - 1 && visited.find(to_string(i) + "," + to_string(j + 1)) == visited.end()) {
-                dfs(i, j + 1);
-            }
-        };
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '1' && visited.find(to_string(i) + "," + to_string(j)) == visited.end()) {
-                    res++;
-                    dfs(i, j);
-                }
-            }
-        }
-        
-        return res;
-    }
-};
+            if i > 0 and (i - 1, j) not in visited:
+                dfs(i - 1, j)
+            if j > 0 and (i, j -1) not in visited:
+                dfs(i, j - 1)
+            if i != m - 1 and (i + 1, j) not in visited:
+                dfs(i + 1, j)
+            if j != n - 1 and (i, j + 1) not in visited:
+                dfs(i, j + 1)
+            
 
+        m = len(grid)
+        n = len(grid[0])
+
+        visited = set()
+        res = 0
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "1":
+                    if (i, j) not in visited:
+                        res += 1
+                        dfs(i, j)
+
+        return res
